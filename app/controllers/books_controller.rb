df8ab564c27e307867a5e4acd00b3068c7643043
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-14 17:08:10 +0900

    デザインの細部を修正 本の検索ページをガラだけ作成 メニューバーに本棚と本の検索を追加

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,5 @@
+class BooksController < ApplicationController
+  def search
+    
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :items
+  get 'books/search'
   get 'home/index'
   root 'home#index'
 end
