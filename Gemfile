2e945b36901c0d61234d32c74955bc9dca3a9993
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-15 13:36:32 +0900

    amazon APIを叩いて書籍のリストを検索して表示する機能を実装(ただし検索キーワードはRubyで固定)

--- Gemfile
+++ Gemfile
@@ -11,7 +11,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'haml-rails'
 gem 'devise'
-
+gem 'amazon-ecs'
+gem 'dotenv-rails'
 
 group :development, :test do
   gem 'byebug'
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'amazon/ecs'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -22,5 +23,12 @@ module Mybooklog
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # amazon-ecs options
+    Amazon::Ecs.configure do |options|
+      options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
+      options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY']
+      options[:associate_tag] = ENV['AWS_ASSOCIATE_TAG']
+    end
   end
 end
