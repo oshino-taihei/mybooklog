988998c2ab735ea2fb5af978a197ec29e92fb50d
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-15 18:14:51 +0900

    キーワード検索に対応(ページ送りはまだ未対応) amazon-ecsでの検索ロジックをコントローラからBookモデル側に持たせるようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,29 +1,6 @@
 class BooksController < ApplicationController
   def search
-    res = Amazon::Ecs.item_search("Ruby",
-           :search_index   => 'Books',
-           :response_group => 'Medium',
-           :country        => 'jp'
-         )
-
-    @books = []
-    res.items.each do |item|
-      asin = item.get('ASIN')
-      url = item.get('DetailPageURL')
-      item_attributes = item.get_element('ItemAttributes')
-      title = item_attributes.get('Title')
-      image = item.get_hash('SmallImage')
-      image_url = image["URL"]
-      authors = item.get_elements('Author').map { |e| e.get_unescaped }
-      publisher = item_attributes.get('Publisher')
-      published_at = item_attributes.get('PublicationDate')
-      list_price = item.get_element('ListPrice')
-      if list_price
-        price = list_price.get('Amount')
-      end
-
-     book = Book.new(asin: asin, url: url, title:title, image: image_url, author: authors.join(', '), publisher: publisher, published_at: published_at, price: price)
-     @books << book
-   end
+    @search_form = SearchForm.new(params[:search_form])
+    @books = Book::search_amazon @search_form
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,31 @@
 class Book < ActiveRecord::Base
-  
+  def self.search_amazon(search_form)
+    res = Amazon::Ecs.item_search(search_form.keyword,
+           search_index:   'Books',
+           response_group: 'Medium',
+           country:        'jp',
+           item_page:      search_form.page
+         )
+    books = []
+    res.items.each do |item|
+      asin = item.get('ASIN')
+      url = item.get('DetailPageURL')
+      item_attributes = item.get_element('ItemAttributes')
+      title = item_attributes.get('Title')
+      image = item.get_hash('SmallImage')
+      image_url = image["URL"] if image
+      author = item.get_elements('Author')
+      authors = author.map { |e| e.get_unescaped }.join(', ') if author
+      publisher = item_attributes.get('Publisher')
+      published_at = item_attributes.get('PublicationDate')
+      list_price = item.get_element('ListPrice')
+      if list_price
+        price = list_price.get('Amount')
+      end
+
+      book = Book.new(asin: asin, url: url, title:title, image: image_url, author: authors, publisher: publisher, published_at: published_at, price: price)
+      books << book
+    end
+  books
+  end
 end
