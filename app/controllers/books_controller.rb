5b80d10deda1194a214e1529edf950ba8df512f0
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-17 21:54:05 +0900

    キーワードなしで検索ページを開いた場合はamazon apiを叩かないようにした

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,11 @@ class BooksController < ApplicationController
 
   def search
     @search_form = SearchForm.new(params[:search_form])
-    page = params[:page] || '1'
-    @books, total = Book::search_amazon(@search_form.keyword, page)
-    @books = Kaminari.paginate_array(@books, total_count: total).page(page).per(10)
+    if params[:search_form]
+      page = params[:page] || '1'
+      @books, total = Book::search_amazon(@search_form.keyword, page)
+      @books = Kaminari.paginate_array(@books, total_count: total).page(page).per(10)
+    end
   end
 
   def show
