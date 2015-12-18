32bcaeb8771ac2a430cc6d3da1d7e0830d3a473d
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-18 10:03:52 +0900

    書籍のAmazon API検索ロジックをリファクタリング

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
 
-  MAX_PAGES = 100 # Amazon Product Advertising APIの仕様におけるItemPageパラメータの上限
+  MAX_ITEM_PAGE = 100 # Amazon Product Advertising APIの仕様におけるItemPageパラメータの上限
 
   # BookのURLでidの代わりにasin使うためにto_keyとto_paramをオーバーライド
   def to_key
@@ -12,44 +12,32 @@ class Book < ActiveRecord::Base
     asin
   end
 
-  # Amazon Product Advertising API にキーワードで書籍検索する
-  # 検索結果をBookテーブルに保存した上で、returnする
   def self.search_amazon(keyword, page)
+    # Amazon Product Advertising API で書籍検索する
     res = Amazon::Ecs.item_search(keyword,
            search_index:   'Books',
            response_group: 'Medium',
            country:        'jp',
            item_page:      page
          )
-    books = []
-    res.items.each do |item|
-      asin = item.get('ASIN')
-      url = item.get('DetailPageURL')
-      item_attributes = item.get_element('ItemAttributes')
-      title = item_attributes.get('Title')
-      image = item.get_hash('SmallImage')
-      image_url = image["URL"] if image
-      author = item.get_elements('Author')
-      authors = author.map { |e| e.get_unescaped }.join(', ') if author
-      publisher = item_attributes.get('Publisher')
-      published_at = item_attributes.get('PublicationDate')
-      list_price = item.get_element('ListPrice')
-      if list_price
-        price = list_price.get('Amount')
-      end
 
-      book = Book.find_or_create_by(asin: asin) do |b|
-        b.url = url
-        b.title = title
-        b.image = image_url
-        b.author = authors
-        b.publisher = publisher
-        b.published_at = published_at
-        b.price = price
+    # 書籍検索結果をBookモデルのリストに変換する
+    # 各書籍データがBookテーブルに登録済みならばテーブルから書籍データを取得し、未登録ならば登録する
+    books = res.items.map do |item|
+      Book.find_or_create_by!(asin: item.get('ASIN')) do |book|
+        item_attributes = item.get_element('ItemAttributes')
+        book.url = item.get('DetailPageURL')
+        book.title = item_attributes.get('Title')
+        book.image = item.get_hash('SmallImage').try!(:fetch, "URL")
+        book.author = item.get_elements('Author').try!(:map) { |e| e.get_unescaped }.join(', ')
+        book.publisher = item_attributes.get('Publisher')
+        book.published_at = item_attributes.get('PublicationDate')
+        book.price = item.get_element('ListPrice').try!(:get, 'Amount')
       end
-      books << book
     end
-  total = [res.total_pages, MAX_PAGES].min
-  [books, total]
+
+    # 検索結果と検索ページ数(API仕様上の最大値を上限とする)を返す
+    total_pages = [res.total_pages, MAX_ITEM_PAGE].min
+    [books, total_pages]
   end
 end
