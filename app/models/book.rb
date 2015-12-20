60fd4f9a270baa626d81d6671d51e4d5326ff905
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-20 17:19:45 +0900

    不要なコメントと不要なto_keyメソッドを削除

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,25 +4,18 @@ class Book < ActiveRecord::Base
 
   MAX_ITEM_PAGE = 100 # Amazon Product Advertising APIの仕様におけるItemPageパラメータの上限
 
-  # BookのURLでidの代わりにasin使うためにto_keyとto_paramをオーバーライド
-  def to_key
-    asin
-  end
+  # BookのURLでidの代わりにasin使うためto_paramをオーバーライド
   def to_param
     asin
   end
 
   def self.search_amazon(keyword, page)
-    # Amazon Product Advertising API で書籍検索する
     res = Amazon::Ecs.item_search(keyword,
            search_index:   'Books',
            response_group: 'Medium',
            country:        'jp',
            item_page:      page
          )
-
-    # 書籍検索結果をBookモデルのリストに変換する
-    # 各書籍データがBookテーブルに登録済みならばテーブルから書籍データを取得し、未登録ならば登録する
     books = res.items.map do |item|
       Book.find_or_create_by!(asin: item.get('ASIN')) do |book|
         item_attributes = item.get_element('ItemAttributes')
@@ -35,8 +28,6 @@ class Book < ActiveRecord::Base
         book.price = item.get_element('ListPrice').try!(:get, 'Amount')
       end
     end
-
-    # 検索結果と検索ページ数(API仕様上の最大値を上限とする)を返す
     total_pages = [res.total_pages, MAX_ITEM_PAGE].min
     [books, total_pages]
   end
