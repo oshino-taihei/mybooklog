3c12962df907f7f002a09468a4ff8b477f82aefb
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-22 21:46:43 +0900

    ヘルパーのメソッド名が不適切だったので修正

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,5 +1,5 @@
 module BooksHelper
-  def review_link_to(book)
+  def review_button_to(book)
     if current_user.books.exists?(book)
       button_to 'レビューを書く', edit_book_review_path(book)
     else
