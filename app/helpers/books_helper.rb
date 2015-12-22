b5f51b47924ccafb6d081b8f03f02570156247a7
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-22 16:18:50 +0900

    検索結果の本棚登録、レビュー更新のデザインをボタン化

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,9 +1,9 @@
 module BooksHelper
   def review_link_to(book)
     if current_user.books.exists?(book)
-      link_to 'レビューを書く', edit_book_review_path(book)
+      button_to 'レビューを書く', edit_book_review_path(book)
     else
-      link_to '本棚に登録', book_review_path(book), method: :post
+      button_to '本棚に登録', book_review_path(book), method: :post
     end
   end
 end
