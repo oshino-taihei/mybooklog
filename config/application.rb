41594516c186bf953f4fdf8332ef899609303372
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-25 10:02:27 +0900

    タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 module Mybooklog
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     config.active_record.raise_in_transactional_callbacks = true
 
     # amazon-ecs options
