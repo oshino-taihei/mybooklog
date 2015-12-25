aa5621756bb74ef063f69973f64ae4e1ca6c6b97
Taihei Oshino <oshino.taihei+git@gmail.com>
2015-12-25 09:01:46 +0900

    Userモデルにユーザ名(name)と自己紹介(description)を追加 登録、更新時にnameを入力できるようにした

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151217021111) do
+ActiveRecord::Schema.define(version: 20151223125931) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -78,6 +78,8 @@ ActiveRecord::Schema.define(version: 20151217021111) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.text     "description"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
