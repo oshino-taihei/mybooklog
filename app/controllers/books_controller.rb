2b2ccca39c217cb85066c326d33d8efe75a2035f
Taihei Oshino <oshino.taihei+git@gmail.com>
2016-01-12 16:03:41 +0900

    Amazon APIでの本検索時のエラーハンドリングに対応

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,8 +5,13 @@ class BooksController < ApplicationController
     @book_search_form = BookSearchForm.new(params[:book_search_form])
     if params[:book_search_form]
       page = params[:page] || '1'
-      @books, total = Book::search_amazon(@book_search_form.keyword, page)
-      @books = Kaminari.paginate_array(@books, total_count: total).page(page).per(10)
+      begin
+        @books, total = Book::search_amazon(@book_search_form.keyword, page)
+        @books = Kaminari.paginate_array(@books, total_count: total).page(page).per(10)
+      rescue Amazon::RequestError => e
+        flash.now[:alert] = "問合せがエラーとなりました。しばらくたってから、再検索して下さい。"
+        logger.error "Amazon Advertising APIへの問合せ中にエラーが発生しました。:#{e}"
+      end
     end
   end
 
