05732fd0599905b79a1ea3f4b7dc67a4a17e1387
Taihei Oshino <oshino.taihei+git@gmail.com>
2016-01-14 10:42:33 +0900

    Amazon ECSのコンフィグをApplicationからconfig/initializersに移動

--- config/application.rb
+++ config/application.rb
@@ -10,12 +10,5 @@ module Mybooklog
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
     config.active_record.raise_in_transactional_callbacks = true
-
-    # amazon-ecs options
-    Amazon::Ecs.configure do |options|
-      options[:AWS_access_key_id] = ENV['AWS_ACCESS_KEY_ID']
-      options[:AWS_secret_key] = ENV['AWS_SECRET_ACCESS_KEY']
-      options[:associate_tag] = ENV['AWS_ASSOCIATE_TAG']
-    end
   end
 end
