7d1a105dd39546d3f9dfd8cf2e9645f33f0b3379
Taihei Oshino <oshino.taihei+git@gmail.com>
2016-01-19 16:55:13 +0900

    フォローまわりの冗長なコードをシンプルにした

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,24 +1,16 @@
 class FollowsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:create]
-  before_action :set_follow, only: [:destroy]
 
   def create
+    @user = User.find(params[:id])
     current_user.follow(@user)
     redirect_to :back
   end
 
   def destroy
+    @follow = Follow.find(params[:id])
     current_user.unfollow(@follow.to_user)
     redirect_to :back
   end
-
-  private
-    def set_user
-      @user = User.find(params[:id])
-    end
-
-    def set_follow
-      @follow = Follow.find(params[:id])
-    end
+  
 end
