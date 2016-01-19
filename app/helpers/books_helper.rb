1ce091c029829c0b253f5801bf10f9ed5a42bb98
Taihei Oshino <oshino.taihei+git@gmail.com>
2016-01-19 18:36:13 +0900

    本の検索結果ページで「本棚登録/レビュー」ボタンの切り替えロジックをよりシンプルにした

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,12 +1,4 @@
 module BooksHelper
-  def update_review_button_to(book)
-    button_to 'レビューを書く', edit_book_review_path(book), method: :get
-  end
-
-  def create_review_button_to(book)
-    button_to '本棚に登録', book_review_path(book), method: :post, remote: true
-  end
-
   def update_button_id(book)
     "book_#{book.asin}_update"
   end
@@ -14,5 +6,4 @@ module BooksHelper
   def create_button_id(book)
     "book_#{book.asin}_create"
   end
-
 end
