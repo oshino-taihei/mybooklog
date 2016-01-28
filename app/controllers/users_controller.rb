68942730db16a029ac5f2a0a6b1eeb9baeb76c19
Taihei Oshino <oshino.taihei+git@gmail.com>
2016-01-28 17:30:54 +0900

    before_actionをなくしてコード量を減らした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,15 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show]
 
   def index
     @users = User.all
   end
 
   def show
+    @user = User.find_by(name: params[:name])
     @review_search_form = ReviewSearchForm.new(params[:review_search_form])
     @reviews = @user.reviews
     @reviews = @reviews.search(@review_search_form) if params[:review_search_form]
   end
-
-  private
-    def set_user
-      @user = User.find_by(name: params[:name])
-    end
+  
 end
